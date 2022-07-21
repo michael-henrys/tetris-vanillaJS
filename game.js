f06b24ad7b00b8344499ce47d4e83fc8f82f102c
michael henrys <michael42h@gmail.com>
2022-07-22 08:35:01 +1200

    add function to move left

--- game.js
+++ game.js
@@ -1,22 +1,25 @@
 let board = []
-speed = 200
+let currentPiece
+speed = 300
 
 
 //create new piece and place it on the board
-newPiece()
+main()
 
-function newPiece() {
-  let piece = createPiece()
-  board.push(piece)
+function main() {
+  currentPiece = createPiece()
+  board.push(currentPiece)
   renderBoard()
   intervalID = setInterval(() => {
-    if(canMoveDown(piece)){
-      console.log('can move down')
-      movePieceDown(piece)
+    if(canMoveDown()){
+      movePieceDown()
     }else{
+      //TODO: check if a row should be cleared
       clearInterval(intervalID)
       if(!checkGameOver()){
-        newPiece()    
+        main()    
+      }else{
+        //TODO: do game over stuff
       }
     }
     renderBoard()
@@ -29,10 +32,10 @@ function createPiece() {
   return {
     color: 'blue', 
     cells: [
-      {x: 0, y: 0},
-      {x: 1, y: 0},
       {x: 2, y: 0},
       {x: 3, y: 0},
+      {x: 4, y: 0},
+      {x: 5, y: 0},
     ]
   }
 }
@@ -44,22 +47,55 @@ function checkGameOver() {
   return false
 }
 
-function canMoveDown(piece) {
+function canMoveDown() {
   //for all the cells in the piece
-  for (let i = 0; i < piece.cells.length; i++) {
-    newY = piece.cells[i].y + 1
-    if (newY >= 15 || board.find(otherPiece => otherPiece !== piece && otherPiece.cells.find(cell => cell.y === newY))) {
+  for (let i = 0; i < currentPiece.cells.length; i++) {
+    newY = currentPiece.cells[i].y + 1
+    if (!isInBounds(currentPiece.cells[i].x, newY) || isOverlap(currentPiece.cells[i].x, newY)) {
       return false
     }
   }
   return true
 }
 
+//function to check if cell is in bounds
+function isInBounds(x, y) {
+  return x >= 0 && x < 10 && y >= 0 && y < 15
+}
+
+//function to check if cell overlaps with other piece
+function isOverlap(x, y) {
+  if(board.find(otherPiece => otherPiece !== currentPiece && otherPiece.cells.find(cell => cell.y === newY))) {
+    return true
+  }
+  return false
+}
+
 //function to move piece down 
-function movePieceDown(piece) {
-  for (let i = 0; i < piece.cells.length; i++) {
-    piece.cells[i].y = newY
+function movePieceDown() {
+  for (let i = 0; i < currentPiece.cells.length; i++) {
+    currentPiece.cells[i].y = newY
   } 
+}
+
+//function to move piece left 
+function movePieceLeft() {
+  console.log('move left')
+  //create a new cells array to store the new positions of the cells in the current piece
+  newCells = []
+  for (let i = 0; i < currentPiece.cells.length; i++) { 
+    //calculate the new position of the x coordinate
+    newX = currentPiece.cells[i].x - 1
+    //check if in bounds and no overlap
+    if (isInBounds(newX, currentPiece.cells[i].y) && !isOverlap(newX, currentPiece.cells[i].y)) {  
+      //add the new position to the new cells array
+      newCells.push({x: newX, y: currentPiece.cells[i].y})  
+    }else{
+      return
+    }
+  }
+  //update the current piece's cells to the new cells array
+  currentPiece.cells = newCells
 }
 
 function renderBoard() {
