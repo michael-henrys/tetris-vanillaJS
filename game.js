0796a44d88f0a928513e9076db714b5a2b7d7ee1
michael henrys <michael42h@gmail.com>
2022-07-21 23:09:41 +1200

    add piece falling logic

--- game.js
+++ game.js
@@ -1,5 +1,32 @@
-board = [
-  {
+let board = []
+speed = 200
+
+
+//create new piece and place it on the board
+newPiece()
+
+function newPiece() {
+  let piece = createPiece()
+  board.push(piece)
+  renderBoard()
+  intervalID = setInterval(() => {
+    if(canMoveDown(piece)){
+      console.log('can move down')
+      movePieceDown(piece)
+    }else{
+      clearInterval(intervalID)
+      if(!checkGameOver()){
+        newPiece()    
+      }
+    }
+    renderBoard()
+  }, speed);
+}
+
+
+//creates a random new piece
+function createPiece() {
+  return {
     color: 'blue', 
     cells: [
       {x: 0, y: 0},
@@ -8,12 +35,40 @@ board = [
       {x: 3, y: 0},
     ]
   }
-]
+}
 
-function renderBoard(board) {
+function checkGameOver() {
+  if(board.find(piece => piece.cells.find(cell => cell.y === 0))) {
+    return true
+  }
+  return false
+}
+
+function canMoveDown(piece) {
+  //for all the cells in the piece
+  for (let i = 0; i < piece.cells.length; i++) {
+    newY = piece.cells[i].y + 1
+    if (newY >= 15 || board.find(otherPiece => otherPiece !== piece && otherPiece.cells.find(cell => cell.y === newY))) {
+      return false
+    }
+  }
+  return true
+}
+
+//function to move piece down 
+function movePieceDown(piece) {
+  for (let i = 0; i < piece.cells.length; i++) {
+    piece.cells[i].y = newY
+  } 
+}
+
+function renderBoard() {
   //get the cells from the document
   let cells = document.getElementsByTagName('td');
-  console.log(cells[0])
+  //clear board
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = 'lightgrey';
+  }
   //for each object on board
   for (var i = 0; i < board.length; i++) {
     //for each cell in the object
@@ -25,5 +80,3 @@ function renderBoard(board) {
     }
   }
 }
-
-renderBoard(board)
